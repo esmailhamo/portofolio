4c4f6f7da0018760e6e6d8022ff330f48ac3d3a6
esmailhamo <149955809+esmailhamo@users.noreply.github.com>
2025-02-12 12:10:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let btn=document.getElementById('btn');
+window.onscroll=function(){
+  if(scrollY>=400)
+  {
+    btn.style.display='block';
+  }
+  else
+  {
+    btn.style.display='none';
+  }
+}
+btn.onclick=function(){
+scroll({
+    top:0,
+    left:0,
+    behavior:"smooth"}
+)
+}
